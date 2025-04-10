6c174adb0e881773d715201d18d5a760f45508a1
hav0x45 <pekkakukonlehto@outlook.com>
2025-04-10 07:27:54 +0300

    Final game!

--- write!.cpp
+++ write!.cpp
@@ -11,20 +11,10 @@
 #define KANJI_MIN   19968
 #define KANJI_MAX   40879
 
-#define writeeng        L"Write!: "
-#define seikaieng       L"Correct!"
-#define incorrecteng    L"Incorrect!"
-#define playagaineng    L"Do you want to play again? (Y/N): "
-
-#define writefi         L"Kirjoita!: "
-#define seikaifi        L"Oikein!"
-#define incorrectfi     L"Väärin!"
-#define playagainfi     L"Haluatko pelata uudestaan? (K/E): "
-
-#define writejp         L"書け!: "
-#define seikaijp        L"正解!"
-#define incorrectjp     L"違う!"
-#define playagainjp     L"もう一回？ (はい/いいえ): "
+#define writestring         L"Write! 書け!: "
+#define correctstring       L"Correct! 正解!"
+#define incorrectstring     L"Incorrect! 違う!"
+#define playagainstring     L"Again? もう一回？ (はい/いいえ): "
 
 
 int main() {
@@ -32,32 +22,6 @@ int main() {
     // Set stdout and stdin mode to WTEXT
     _setmode(_fileno(stdout), _O_WTEXT);
     _setmode(_fileno(stdin), _O_WTEXT);
-
-    std::wstring writestring;
-    std::wstring correctstring;
-    std::wstring incorrectstring;
-    std::wstring playagainstring;
-
-    std::wcout << L"Select language (en|fi|jp?): ";
-    std::wstring language;
-    std::wcin >> language;
-
-    if (language == L"en") {
-        writestring     = writeeng;
-        correctstring    = seikaieng;
-        incorrectstring = incorrecteng;
-        playagainstring = playagaineng;
-    } else if (language == L"fi") {
-        writestring     = writefi;
-        correctstring    = seikaifi;
-        incorrectstring = incorrectfi;
-        playagainstring = playagainfi;
-    } else if (language == L"jp") {
-        writestring     = writejp;
-        correctstring    = seikaijp;
-        incorrectstring = incorrectjp;
-        playagainstring = playagainjp;
-    }
 
     bool running = true;
 
@@ -79,62 +43,43 @@ int main() {
 
         srand(gen());
         int randomkanjinumber = rand() % 2135;
+        int lines = 0;
 
-        for (int lines = 0; getline(kanjifile, kanjiline); lines++) {
+        for (lines = 0; getline(kanjifile, kanjiline); lines++) {
             // if lines = randomnumber store it to the write variable
             if (lines == randomkanjinumber) {
                 kanji = kanjiline;
+                std::wcout << "Jouyou kanji #" << lines << " " << kanji << std::endl;
+                
             }
         }
 
         // Variable for input
         std::wstring write;
 
-        std::wcout << kanji;
+        std::wcout << writestring << std::endl;
 
         std::wstring usertest;
 
         std::wcin >> write;
         if (write == kanji) {
             std::wcout << correctstring << std::endl;
+            std::wcout << "--------------------------" << std::endl;
         } else {
             std::wcout << incorrectstring << std::endl;
+            std::wcout << "--------------------------" << std::endl;
             while (again != true) {
                 std::wcout << playagainstring;
                 std::wcin >> usertest;
-                if (language == L"en") {
-                    if (usertest == L"Y" || usertest == L"y") {
-                        again = true;
-                    } else if (usertest == L"N" || usertest == L"n") {
-                        running = false;
-                        return 0;
-                    } else {
-                        std::wcout << L"I didn\'t understand that..." << std::endl;
-                    }
-                }
 
-                else if (language == L"fi") {
-                    if (usertest == L"K" || usertest == L"k") {
-                        again = true;
-                    } else if (usertest == L"E" || usertest == L"e") {
-                        running = false;
-                        return 0;
-                    } else {
-                        std::wcout << L"En ymmärtänyt tuota..." << std::endl;
-                    }
+                if (usertest == L"はい") {
+                    again = true;
+                } else if (usertest == L"いいえ") {
+                    running = false;
+                    return 0;
+                } else {
+                    std::wcout << L"それは理解できなかった。。。" << std::endl;
                 }
-
-                if (language == L"jp") {
-                    if (usertest == L"はい") {
-                        again = true;
-                    } else if (usertest == L"いいえ") {
-                        running = false;
-                        return 0;
-                    } else {
-                        std::wcout << L"それは理解できなかった。。。" << std::endl;
-                    }
-                }
-
             }
         }
     }
